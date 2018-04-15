f69c84d7eab5d4d548639105ec0a6eacacaabb08
thenamesdave <dmmarulli@gmail.com>
2018-04-15 16:28:57 -0700

    added brain animation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 function drawBrain() {
-  var imageElement = document.querySelector('#brain');
-  var chromata = new Chromata(imageElement);
+  var imageElement = document.querySelector('#brain'),
+      chromata;
 
-  chromata.start();
+  chromata = new Chromata(imageElement, {
+    pathFinderCount: 600,
+    speed: 20,
+    turningAngle: Math.PI*(3/2),
+    colorMode: 'grayscale',
+    lineWidth: 1,
+    lineMode: '[point]',
+    compositeOperation: 'lighten',
+    origin: ['bottom','50% 50%','top'],
+    outputSize: 'container', // original, container
+    key: 'low',
+    backgroundColor: 'hsla(34, 70%, 70%, 0)'
+});
+chromata.start();
 }
 
 
